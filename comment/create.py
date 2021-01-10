00673ace08bfb974b9891ee5fb79a30311601fa5
radatorn <radatorn.t@gamil.com>
2021-01-10 19:53:31 +0700

    เพิ่มและแสดงผล ชื่อ นามสกุล อายุ น้ำหนัก

---
+++ comment/create.py
@@ -0,0 +1,15 @@
+name = "radatron thanaditsakun"
+age = 22
+weight = 47
+# boolen | can is has
+can_walk = True
+can_run = False
+# is_dog
+# is_cat
+# has_pan
+
+print(name)
+print(age)
+print(weight)
+print(can_walk)
+print(can_run)
