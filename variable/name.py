d5352314d4bd41eb1e769e687b62a904a9d85805
radatorn <radatorn.t@gamil.com>
2021-01-10 21:46:26 +0700

    การประกาศตัวแปลที่สามารถประกาศได้ และ ไม่ได้

---
+++ variable/name.py
@@ -0,0 +1,12 @@
+#สามารถประกาศได้ 
+myvar = "Gluay"
+my_var = "Gluay"
+_my_var = "gluay"
+myVar = "Gluay"
+MYVAR = "Gluay
+myvar2 = "Gluay"
+
+#ไม่สามารถประกาศได้
+2myvar =" Gluay"
+my-var = "gluay"
+my var = "gluay"
