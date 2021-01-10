8c06425a0627cd3835181328c514733e401ad739
radatorn <radatorn.t@gamil.com>
2021-01-10 20:56:57 +0700

    เพิ่มแสดงผล[-]python isfantastic                     python is awesome

---
+++ variable/global_variable2.py
@@ -0,0 +1,13 @@
+x = "awesome"
+
+
+def myfunc():
+    x = "fantastic"
+    print("python is " + x)
+
+
+myfunc()
+print("python is " + x)
+
+# output:[-]python isfantastic
+# output: python is awesome
