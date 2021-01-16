c925ee1ab266bd4ebe4fd75fbce395a7dc6454c2
radatorn <radatorn.t@gamil.com>
2021-01-16 16:00:50 +0700

    เป็นการใช้คำสั่ง string print(string.upper())  "HELLO , WROLD! " เป็นตัวพิมพ์ใหญ่ทั้งหมด print(string.lower())   "hello , world!" เป็นตัวพิมพ์เล็กทั้งหมด print(string.strip())  "Hello , World!" ตัวนำหน้าเป็นตัวพิมพ์ใหญ่ print(string.replace("H", "J"))  Hello , World!   ตัวนำหน้าเป็นตัวพิมพ์ใหญ่ แล้วครั้นด้วย คอมม่า print(string.split(","))   ['Hello','World!'] มี [ ] ครอบ และวตัวนำหน้าเป็นพิมพ์ใหญ่

---
+++ string/modify strings.py
@@ -0,0 +1,7 @@
+string = "Hello, World!"
+print(string.upper())  # Output : "HELLO , WROLD!"
+print(string.lower())  # Output : "hello , world!"
+print(string.strip())  # Output : "Hello , World!"
+print(string.replace("H", "J"))  # Output : Fello , World!
+print(string.split(","))  # Output : ['Hello','World!']
+print(len(string))  # 15
