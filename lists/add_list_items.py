745899ebb48d9cc91decace4029dcb402abedca0
radatorn <radatorn.t@gamil.com>
2021-01-17 15:38:26 +0700

    no message

---
+++ lists/add_list_items.py
@@ -0,0 +1,9 @@
+# EXAMPLE 1
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+
+print(thislist)  # Output: ["apple","banana","cherry","orange"]
+
+# EXAMPLE 2
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")  # Output:  ["apple","orange","banana","cherry"]
---
+++ lists/change _item_value.py
@@ -0,0 +1,4 @@
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "watermalon"
+
+print(thislist)  # Output : ["apple","watermalon","cherry"]
