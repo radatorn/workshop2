86a8fe84a49b3399fb3dc8022463dae2ad687258
radatorn <radatorn.t@gamil.com>
2021-03-18 18:32:11 +0700

    no message

---
+++ class/2.py
@@ -0,0 +1,10 @@
+name = "radatorn thanaditsakun"
+age = 22
+address = "Bangplee"
+
+Person = Person(name, age, address)
+
+print(Person.get_name())
+print(Person.get_age())
+print(Person.get_address())
+print(Person.info())
