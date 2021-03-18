6843123fc18c7824da53a5bd7e045b87d1909b2a
radatorn <radatorn.t@gamil.com>
2021-03-18 21:41:04 +0700

    no message

---
+++ Dict/Add_Dictionary_item.py
@@ -0,0 +1,8 @@
+thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964}
+model = thisdict["color"] = "red"
+print(thisdict)
+
+thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964}
+model = thisdict.update({"color": "red"})
+print("model:", model)
+© 2021 GitHub, Inc.
